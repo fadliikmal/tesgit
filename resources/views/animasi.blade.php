1ba96ce53edd871b3e290abf43145a9010c11e85
Amumtz <amumtaziah@gmail.com>
2025-04-23 22:51:14 +0700

    tes tampilan loading

---
+++ resources/views/animasi.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lottie Animasi</title>
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</head>
+<body style="margin: 0; display: flex; justify-content: center; align-items: center; height: 100vh; background: #f5f5f5;">
+
+    <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_usmfx6bp.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;" loop autoplay></lottie-player>
+
+</body>
+</html>
